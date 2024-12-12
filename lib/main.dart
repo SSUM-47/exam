3cd024aecd3d817d41149a957fcc30b0ec63bf03
SSUM-47 <44286957justin@gmail.com>
2024-12-12 22:36:50 +0800

    aaa

---
+++ lib/main.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+final player=AudioPlayer()..setReleaseMode(ReleaseMode.loop);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ClipPath example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green,),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final tabs=[
+    Screen1(),
+    Screen2(),
+    Screen3(),
+    Screen4(),
+  ];
+
+  int previousIndex=0;
+  int currentIndex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentIndex==0) player.play(AssetSource("1.mp3"));
+    return Scaffold(
+      appBar: AppBar(title: Text('我的自傳'),
+        centerTitle: true,),
+      body: tabs[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        selectedFontSize: 18,
+        unselectedFontSize: 14,
+        iconSize: 30,
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: currentIndex==0?Image.asset('assets/f1.jpg', width: 40, height: 40,):Image.asset('assets/f1.jpg', width: 30, height: 30,), label: '自我介紹',),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: '學習歷程',),
+          BottomNavigationBarItem(icon: Icon(Icons.scale_outlined), label: '學習計畫',),
+          BottomNavigationBarItem(icon: Icon(Icons.engineering), label: '專業方向',),
+        ],
+        onTap: (index) {
+          setState(() {
+            previousIndex=currentIndex;
+            currentIndex=index;
+            if (index==0) {
+              if (previousIndex==currentIndex) player.resume();
+              player.stop();
+              player.play(AssetSource("1.mp3"));
+            }
+            if (index==1) {
+              if (previousIndex==currentIndex) player.resume();
+              player.stop();
+              player.play(AssetSource("2.mp3"));
+            }
+            if (index==2) {
+              if (previousIndex==currentIndex) player.resume();
+              player.stop();
+              player.play(AssetSource("3.mp3"));
+            }
+            if (index==3) {
+              if (previousIndex==currentIndex) player.resume();
+              player.stop();
+              player.play(AssetSource("4.mp3"));
+            }
+          });
+        },
+      ),
+
+    );
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  Screen1({super.key});
+
+  String s1='我出生在一個普通的家庭，家裡有爸爸、媽媽、阿公、阿嬤跟我，平時我喜歡打遊戲，自認有盡到學生的本分。'
+      '在小學時代的我很活潑、很好動，在課業上表現平平，交了很多朋友，雖然現在基本已經沒有任何聯絡了。'
+      '國中，對於一些危險甚至犯法的行進不理解，所以對班上的主要同學群感到厭惡，以至於在這時期過得不太順遂'
+      '為了脫離國中那種社群認真念書考進了一所不錯的高中，在這裡交到了更多朋友，雖然高中的課業並不是那麼理想，但還是靠著不錯的數學能力支撐著成績，並在這樣的狀況下進行各種社交活動，即使現在大三了，也很常跟著高中同學出門或一起打線上遊戲，是很令人開心的三年。';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //放標題
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+            child: Text('Who am I', style: TextStyle(fontSize: 24,
+                fontWeight: FontWeight.bold),),
+          ),
+          //自傳部分
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width:3),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(color: Colors.amberAccent, offset: Offset(6, 6)),
+              ],
+            ),
+            child: Text(s1, style: TextStyle(fontSize: 20),),
+          ),
+          SizedBox(height: 15,),
+          Container(
+            color: Colors.redAccent,
+            child: Image.asset('assets/f1.jpg'),
+            height: 200,
+            width: 200,
+          ),
+          SizedBox(height: 30,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width:2, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('assets/f1.jpg'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(width: 10,),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width:2, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('assets/f1.jpg'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Screen2'),
+      ),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('大一時期'),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                child: ListView(
+                  children: [
+                    Text('1. 學好英文'),
+                    Text('2. 組合語言'),
+                    Text('3. 考取證照'),
+                    Text('4. 人際關係'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(),
+          SizedBox(height: 10,),
+          Row(),
+          SizedBox(height: 10,),
+          Row(),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Screen4'),
+      ),
+    );
+  }
+}
